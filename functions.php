e60c30faaef343ee9ff78c1d9732fcd1a463be42
Rafael Birkmann <mail@rafaelbirkmann.de>
2015-06-11 16:39:18 +0200

    cpt

--- functions.php
+++ functions.php
@@ -172,4 +172,58 @@
 
 	/* Contact Form End */
 
+
+	/* Custom Post Type Start */
+
+	 
+	function ah_custom_post_type() {
+	 
+		$labels = array(
+			'name' => 'Portfolio Items',
+			'singular_name' => 'Portfolio',
+			'menu_name' => 'CPT',
+			'parent_item_colon' => '',
+			'all_items' => 'All Items',
+			'view_item' => 'View Item',
+			'add_new_item' => 'Add New Item',
+			'add_new' => 'Add New',
+			'edit_item' => 'Edit Item',
+			'update_item' => 'Update Item',
+			'search_items' => '',
+			'not_found' => '',
+			'not_found_in_trash' => '',
+		);
+
+	$rewrite = array(
+		'slug' => 'portfolio',
+		'with_front' => true,
+		'pages' => true,
+		'feeds' => true,
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail', 'comments', 'trackbacks', ),
+		'taxonomies' => array( 'category', 'post_tag' ),
+		'hierarchical' => false,
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_nav_menus' => true,
+		'show_in_admin_bar' => true,
+		'menu_position' => 5,
+		'can_export' => false,
+		'has_archive' => true,
+		'exclude_from_search' => false,
+		'publicly_queryable' => true,
+		'rewrite' => $rewrite,
+		'capability_type' => 'page',
+	);
+
+	register_post_type( 'portfolio', $args );
+	 
+	}
+	 
+	add_action( 'init', 'ah_custom_post_type', 0 );
+
 ?>
---
+++ single-portfolio.php
@@ -0,0 +1,16 @@
+<?php 
+
+	/*
+	Template Name: Archive Portfolio Template
+	*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="wrapper">
+	<h2>single-portfolio.php</h2>
+	<?php query_posts(array('post_type'=>'portfolio')); ?>
+</div>
+
+<?php get_footer(); ?>
