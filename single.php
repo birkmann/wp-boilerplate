89e000dfc6dc68639b3fed6977bdd150efe379f1
Rafael Birkmann <mail@rafaelbirkmann.de>
2015-06-16 13:23:03 +0200

    comments

--- single.php
+++ single.php
@@ -16,6 +16,20 @@
 				<article class="single">
 					<header>
 						<h1><?php the_title(); ?></h1>
+						<ul class="infos">
+							<li>
+								<span class="icon flaticon flaticon-calentar"></span>
+								<time class="number" datetime="<?php the_time('Y-m-d'); ?>" pubdate>
+									<?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?>
+								</time>
+							</li>
+							<li>
+								<span class="icon flaticon flaticon-speechbubble65"></span>
+								<span class="number">
+									<?php comments_number( '0', '1', '%' ); ?>
+								</span>
+							</li>
+						</ul>
 					</header>
 					<?php the_content(); ?>
 					<?php comments_template(); ?>
